58390ee11680c350408347ccf6f149378f4c8a02
Xavier Van de Woestyne <xavier.vdw@gmail.com>
2015-02-07 19:36:06 +0100

    Update pages

--- custom.js
+++ custom.js
@@ -3,13 +3,20 @@ var content = document.getElementById('innercontent');
 var childs  = content.querySelectorAll('h1,h2,h3,h4,h5,h6');
 var toc     = document.createElement('select');
 var target  = document.getElementById('rtitle');
+
+option  = document.createElement('option');
+option.value = 'top';
+textnd  = document.createTextNode("Remonter l'article");
+option.appendChild(textnd);
+toc.appendChild(option);
+
 for(i = 0; i < childs.length; i++) {
 		current = childs.item(i);
 		textnd  = document.createTextNode(current.firstChild.data);
 		option  = document.createElement('option');
 		option.value = current.getAttribute('id');
 		option.appendChild(textnd);
-		toc.appendChild(option)
+		toc.appendChild(option);
 }
 toc.addEventListener('change', function (e) {
 		window.location = "#"+(e.target.options[e.target.selectedIndex].value)
